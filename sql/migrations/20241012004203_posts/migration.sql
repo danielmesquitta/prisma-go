e26cc2b47121b324f3f088aa3bc3f3d9826dc70a
Daniel Mesquita <danielmesquitta123@gmail.com>
2024-10-11 21:42:36 -0300

    add deleted_at, created_at and updated_at

--- sql/migrations/20241012004203_posts/migration.sql
+++ sql/migrations/20241012004203_posts/migration.sql
@@ -1,8 +1,11 @@
 -- CreateTable
 CREATE TABLE "posts" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "published" BOOLEAN NOT NULL DEFAULT true,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
 
     CONSTRAINT "posts_pkey" PRIMARY KEY ("id")
 );
